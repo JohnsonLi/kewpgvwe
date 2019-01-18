e30c73375af6cf457c441323922f116c503e1fe0
AnthonyLam <alam6@stuy.edu>
2019-01-18 13:57:09 -0500

    forking works

--- select_server.c
+++ select_server.c
@@ -52,104 +52,107 @@ int main() {
   fd_set read_fds;
 
   listen_socket = server_setup();
-  client_socket = server_connect(listen_socket);
-  f = fork();
-  if (f==0){
-    int acc = 0;
-    int err;
-    char username[100];
-    char password[100];
-    char answer[100];
-    char other_person[100];
-    char login_message[100];
-    char register_message[100];
-    memset(username, 0, 100);
-    memset(password, 0, 100);
-    memset(answer, 0, 100);
-    memset(other_person, 0, 100);
-    memset(login_message, 0, 100);
-    memset(register_message, 0, 100);
+  while(1){
+    client_socket = server_connect(listen_socket);
+    f = fork();
+    if (f==0){
+      int acc = 0;
+      int err;
+      char username[100];
+      char password[100];
+      char answer[100];
+      char other_person[100];
+      char login_message[100];
+      char register_message[100];
+      memset(username, 0, 100);
+      memset(password, 0, 100);
+      memset(answer, 0, 100);
+      memset(other_person, 0, 100);
+      memset(login_message, 0, 100);
+      memset(register_message, 0, 100);
 
-    printf("%s\n", strerror(errno));
+      printf("%s\n", strerror(errno));
 
-    read(client_socket, answer, 1000);
-    printf("[Server] answer: %s\n", answer);
-    read(client_socket, username, 100);
-    printf("[Server] username: %s\n", username);
-    read(client_socket,password,100);
-    printf("[Server] password: %s\n", password);
-    if(answer[0] == 'y'){
-      if(check_account(username,password)){
-        printf("Correct/n");
-      }
-      else{
-        printf("Incorrect\n");
-      }
-    }
-    else{
-      int file = open("users.txt",O_RDWR | O_APPEND);
-      int a = write(file,username,strlen(username));
-      if( a <0 ){
-        printf("%s \n",strerror(errno));
-      }
-      int b = write(file,password,strlen(password));
-      if(b< 0 ){
-        printf("%s\n",strerror(errno));
-      }
-      read(client_socket, other_person,100);
-      int chat_file;
-      char* result = malloc(50);
-      char *chatroom = malloc(300);
-      sprintf(chatroom, "%s_%s.txt", username, other_person);
-      chat_file = open(chatroom, O_RDWR | O_APPEND);
-      if(chat_file < 0){
-        sprintf(chatroom, "%s_%s.txt", other_person, username);
-        chat_file = open(chatroom, O_RDWR | O_APPEND);
-        if(chat_file < 0){
-          chat_file = open(chatroom, O_CREAT | O_RDWR | O_APPEND, 0644);
-          if (chat_file>0){
-            strcpy(result,"[Server] Created new chat\n");
-          }
-          else{
-            strcpy(result,"[Server] Failed\n");
-          }
+      read(client_socket, answer, 1000);
+      printf("[Server] answer: %s\n", answer);
+      read(client_socket, username, 100);
+      printf("[Server] username: %s\n", username);
+      read(client_socket,password,100);
+      printf("[Server] password: %s\n", password);
+      if(answer[0] == 'y'){
+        if(check_account(username,password)){
+          printf("Correct/n");
         }
         else{
-	  err = stat(chatroom, &file_stat);
-	  last_modified = file_stat.st_mtime;
-          strcpy(result,"[Server] Found chat\n");
+          printf("Incorrect\n");
         }
       }
       else{
-	err = stat(chatroom, &file_stat);
-	last_modified = file_stat.st_mtime;
+        int file = open("users.txt",O_RDWR | O_APPEND);
+        int a = write(file,username,strlen(username));
+        if( a <0 ){
+          printf("%s \n",strerror(errno));
+        }
+        int b = write(file,password,strlen(password));
+        if(b< 0 ){
+          printf("%s\n",strerror(errno));
+        }
+        read(client_socket, other_person,100);
+        int chat_file;
+        char* result = malloc(50);
+        char *chatroom = malloc(300);
+        sprintf(chatroom, "%s_%s.txt", username, other_person);
+        chat_file = open(chatroom, O_RDWR | O_APPEND);
+        if(chat_file < 0){
+          sprintf(chatroom, "%s_%s.txt", other_person, username);
+          chat_file = open(chatroom, O_RDWR | O_APPEND);
+          if(chat_file < 0){
+            chat_file = open(chatroom, O_CREAT | O_RDWR | O_APPEND, 0644);
+            if (chat_file>0){
+              strcpy(result,"[Server] Created new chat\n");
+            }
+            else{
+              strcpy(result,"[Server] Failed\n");
+            }
+          }
+          else{
+           err = stat(chatroom, &file_stat);
+           last_modified = file_stat.st_mtime;
+           strcpy(result,"[Server] Found chat\n");
+         }
+       }
+       else{
+        err = stat(chatroom, &file_stat);
+        last_modified = file_stat.st_mtime;
         strcpy(result,"[Server] Found chat\n");
       }
       printf("%s %s\n", result, chatroom);
       write(client_socket, result, 50);
       int in;
       char* buffer = calloc(1,100);
-
       while (1) {
-	if (in = read(client_socket, buffer, 100)){
-	  write(chat_file, buffer, strlen(buffer));
-	  while(in = read(client_socket, buffer, 100)){
-	     write(chat_file, buffer, strlen(buffer));
-	  }
-	}
-	if(file_is_modified(chatroom, last_modified)){
-	  err = stat(chatroom, &file_stat);
-	  last_modified = file_stat.st_mtime;
-	  while(in = read(chat_file, buffer, 100)){
-	    write(client_socket, buffer, in);
-	  }
-	}
-      }
-    }
-  }
-  else{
-    close(client_socket);
-  }
+       if (in = read(client_socket, buffer, 100)){
+         write(chat_file, buffer, strlen(buffer));
+         printf("%s\n", buffer);
+         while(in = read(client_socket, buffer, 100)){
+           write(chat_file, buffer, strlen(buffer));
+           printf("%s\n", buffer);
+         }
+       }
+       if(file_is_modified(chatroom, last_modified)){
+        err = stat(chatroom, &file_stat);
+        last_modified = file_stat.st_mtime;
+        while(in = read(chat_file, buffer, 100)){
+         write(client_socket, buffer, in);
+       }
+     }
+   }
+ }
+}
+else{
+  close(client_socket);
+}
+}
 }
 
 
